9d637ed761e4bd66aa78fce89b44a36716afca8c
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-17 21:03:08 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения GearMagic.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Marketplace',
+    '{description}' => 'Installing ready-made modules and extensions',
+    '{permissions}' => [
+        'any'       => ['Full access', 'Access to Marketplace extensionss'],
+        'extension' => ['Extension', 'Access to Marketplace module extensions']
+    ],
+];
