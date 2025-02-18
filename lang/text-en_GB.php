4992334c54a72ca6364467cdb3efe6a385d7483d
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-02-18 11:05:35 +0300

    Пакет английской (британской) локализации

--- lang/text-en_GB.php
+++ lang/text-en_GB.php
@@ -10,8 +10,8 @@
  */
 
 return [
-    '{name}'        => 'Marketplace',
-    '{description}' => 'Installing ready-made modules and extensions',
+    '{name}'        => 'Application Marketplace',
+    '{description}' => 'Installation of ready-made solutions (components, themes, etc.)',
     '{permissions}' => [
         'any'       => ['Full access', 'Access to Marketplace extensionss'],
         'extension' => ['Extension', 'Access to Marketplace module extensions']
